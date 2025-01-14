b2243177b9f7745a03d9f5535c7bb6d760559e69
GrahaNirmala <simgn.grahanirmala@gmail.com>
2025-01-14 21:10:40 +0700

    fix api get data occupant

--- src/app/api/v1/occupant/[id]/document/route.ts
+++
@@ -1,6 +0,0 @@
-import { GET } from "./+get"
-
-// If route only contains GET method, it will be static, but we use req.headers on getToken and it will be warning on build
-export const dynamic = "force-dynamic"
-
-export { GET }
